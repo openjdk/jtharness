0684c6e628f639946c55f7a410dbfee31958a75e
golla.yadav@oracle.com <gryadav@saturn.s3.javaplatfo1phx.oraclevcn.com>
2023-02-12 20:37:33 -0800

    7903376: Feature Tests - Adding five JavaTest GUI legacy automated test scripts

---
+++ gui-tests/src/gui/src/jthtest/Markers/Markers15.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001, 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package jthtest.Markers;
+
+import java.lang.reflect.InvocationTargetException;
+import jthtest.Test;
+import jthtest.tools.ConfigDialog;
+import jthtest.tools.Configuration;
+import jthtest.tools.JTFrame;
+
+public class Markers15 extends Test {
+
+	public void testImpl() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException {
+		mainFrame = new JTFrame(true);
+
+		mainFrame.openDefaultTestSuite();
+		addUsedFile(mainFrame.createWorkDirectoryInTemp());
+		Configuration configuration = mainFrame.getConfiguration();
+		configuration.load(CONFIG_NAME, true);
+
+		ConfigDialog config = configuration.openByKey();
+
+		int[] indexes = new int[] { 4, 5, 6, 8, 9 };
+		config.getBookmarks_EnableBookmarks().push();
+
+		config.setBookmarkedByMenu(indexes);
+		String[] namesAll = config.getElementsNames();
+		config.getBookmarks_ShowOnlyBookmarkedMenu().push();
+		String namesHidden[] = config.getElementsNames();
+
+		config.openGroupByPopup(namesAll, namesHidden);
+	}
+
+}
