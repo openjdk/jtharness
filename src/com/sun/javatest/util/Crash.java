d527358439a4b2434d89c5ce53d353db2ebbdc14
jandrlik <jandrlik@redhat.com>
2023-08-28 12:27:38 +0200

    storing crash file object rather than its contents in the crash object

--- src/com/sun/javatest/util/Crash.java
+++ src/com/sun/javatest/util/Crash.java
@@ -11,33 +11,19 @@ import java.util.List;
  */
 public class Crash {
     private String pid;
-    private List<String> lines;
+    private File file;
 
 
     public Crash(TestDescription td, String hs_err, String pid) {
-        File hs_err_File = new File(td.getDir(), hs_err);
         this.pid = pid;
-        List <String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(hs_err_File))){
-            String line;
-            // Read all lines from the file
-            while ((line = reader.readLine()) != null) {
-                lines.add(line);
-            }
-        } catch (
-                FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        this.lines = lines;
+        this.file = new File(td.getDir(), hs_err);
     }
 
     public String getPid() {
         return pid;
     }
 
-    public List<String> getLines() {
-        return lines;
+    public File getFile() {
+        return file;
     }
 }
