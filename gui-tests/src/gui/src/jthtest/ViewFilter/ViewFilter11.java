a573efcddc65d6bfa772f9ab1de476e2b1c05d66
Golla Yadav <golla.yadav@oracle.com>
2024-07-10 07:48:45 -0700

    7903773: Feature Tests - Adding four JavaTest GUI legacy automated test scripts

---
+++ gui-tests/src/gui/src/jthtest/ViewFilter/ViewFilter11.java
@@ -0,0 +1,63 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2009, 2010, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+/*
+ * To change this template, choose Browse | Templates
+ * and open the template in the editor.
+ */
+package jthtest.ViewFilter;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.netbeans.jemmy.operators.JDialogOperator;
+
+/**
+ *
+ * @author naryl
+ */
+public class ViewFilter11 extends ViewFilter {
+
+     public static void main(String[] args) {
+          JUnitCore.main("jthtest.gui.ViewFilter.ViewFilter11");
+     }
+
+     @Test
+     public void testViewFilter11() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException {
+//
+//          startWithDefaultWorkdir();
+//
+//          selectFilter(mainFrame, "Custom");
+//
+//          setKeywordFilter(mainFrame, 1, "kk6");
+//
+//          new JDialogOperator("Error").close();
+//
+     }
+
+}
+
