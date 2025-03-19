d48bb40a22e4e0ab0c3ca80bfc8eee8cb746cbc5
Golla Yadav <golla.yadav@oracle.com>
2025-03-19 21:05:15 +0530

    Updated TT_SelectionCE8.java

--- gui-tests/src/gui/src/jthtest/TestTree/TT_SelectionCE8.java
+++ gui-tests/src/gui/src/jthtest/TestTree/TT_SelectionCE8.java
@@ -26,6 +26,7 @@
  */
 package jthtest.TestTree;
 
+import jthtest.TT_SelectionCE;
 import jthtest.tools.ConfigDialog;
 import jthtest.tools.ConfigDialog.QuestionTree;
 import jthtest.tools.Configuration;
